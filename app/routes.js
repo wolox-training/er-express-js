2aadaf2ff9175ca8379988025a29db5bfb1abdfe
eroa9612 <edilberto_roava@fet.edu.co>
2021-09-29 12:13:00 -0500

    endpoint to get weets

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,9 @@
 // const controller = require('./controllers/controller');
-const { healthCheck } = require('./controllers/healthCheck');
+const { healthCheck, witter } = require('./controllers/healthCheck');
 
 exports.init = app => {
   app.get('/health', healthCheck);
+  app.get('/weets', witter);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
