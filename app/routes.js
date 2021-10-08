48688a305885c1989dd6700863ff744b8f54620a
eroa9612 <edilberto_roava@fet.edu.co>
2021-10-08 13:55:15 -0500

    resolve merge

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,7 @@
-import user from './controllers/user';
-// const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.post('/users', user.add);
   // app.get('/endpoint/get/path', [], controller.methodGET);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
