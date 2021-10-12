6f3744db4b4ef2ee40cdd431672869d260b6d409
eroa9612 <edilberto_roava@fet.edu.co>
2021-10-12 15:33:11 -0500

    unit test user exist

--- test/signup.test.js
+++ test/signup.test.js
@@ -1,25 +1,39 @@
 const supertest = require('supertest');
 const app = require('../app');
+const { USER_ERROR, PASSWORD_ERROR } = require('../config/messageError');
+const { USER_ERROR: USER_CODE, PASSWORD_ERROR: PASSWORD_CODE } = require('../app/errors');
 
 const api = supertest(app);
 
 describe('User registration controller', () => {
-  test('create user', async () => {
+  test('create user', async done => {
     const user = { email: 'edilberto@wolox.co', name: 'Edilberto', lastName: 'Roa', password: '12345678e' };
     await api
       .post('/users')
       .send(user)
       .set('Accept', 'application/json')
-      .expect(201)
+      .expect(201, { name: user.name, lastName: user.lastName, email: user.email })
+      .expect('Content-Type', /application\/json/);
+    done();
+  });
+  test('user already exists', async () => {
+    const user = { email: 'edilberto@wolox.co', name: 'Edilberto', lastName: 'Roa', password: '12345678e' };
+    await api.post('/users').send(user);
+    await api
+      .post('/users')
+      .send(user)
+      .set('Accept', 'application/json')
+      .expect(422, { message: USER_ERROR, internal_code: USER_CODE })
       .expect('Content-Type', /application\/json/);
   });
+
   test('password invalid', async done => {
     const user = { email: 'edilberto@wolox.co', name: 'Edilberto', lastName: 'Roa', password: '1234567' };
     await api
       .post('/users')
       .send(user)
       .set('Accept', 'application/json')
-      .expect(422)
+      .expect(422, { message: PASSWORD_ERROR, internal_code: PASSWORD_CODE })
       .expect('Content-Type', /application\/json/);
     done();
   });
